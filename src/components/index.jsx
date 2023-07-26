0b3fe9dffc1f17d0cde21aede88e77559b517929
Daniel <daniel0ar@hotmail.com>
2023-07-25 23:06:45 -0500

    feat: Add search bar and filter with axios

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,40 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 
 import MovieList from "./movie-list";
+import SearchBar from "./search-bar";
 
-class Index extends Component {
-  
-    constructor() {
-      super();
-      this.state = {movies: [], selectedMovie: null};
-      this.movieSearch("");
-    }
-  
-    movieSearch(term) {
-      axios.get('http://localhost:3031/movies')
-      .then(res => 
-          this.setState({
-            movies: res.data,
-            selectedMovie: res.data[0]
-          })
-        ).catch(err => console.log(err))
-    }
-  
-    render() {
-      return (
+const Index = () => {
+
+    const [ movies, setMovies] = useState([]);
+    const [ selectedMovie, setSelectedMovie] = useState();
+
+    const client = axios.create({
+        baseURL: "http://localhost:3031/movies" 
+    });
+
+    useEffect(() => {
+        client.get('?_limit=10').then((res) => {
+           setMovies(res.data);
+        });
+     }, []);
+    
+
+    const movieSearch = (term) => {
+        client.get(`?title_like=${term}`)
+            .then((res) => {
+                setMovies(res.data);
+            }).catch(err => console.log(err))
+    };
+
+    const movieSearchWait = _.debounce((term) => {movieSearch(term)},300);
+
+    return (
         <div className="container mx-auto px-4">
-          <h1>Orion Movies</h1>
-          <a type="button" href="/new"
-            className="cursor-pointer py-2 px-3 rounded-md shadow bg-indigo-600 hover:bg-indigo-700">
-            New
-          </a>
-          <MovieList
-            onMovieSelect={selectedMovie => this.state = {selectedMovie}} 
-            movies={this.state.movies}></MovieList>
+            <h1>Orion Movies</h1>
+            <a type="button" href="/new"
+                className="cursor-pointer py-2 px-3 rounded-md shadow bg-indigo-600 hover:bg-indigo-700">
+                New
+            </a>
+            <SearchBar onSearchTermChange={movieSearchWait}></SearchBar>
+            <MovieList
+                onMovieSelect={selectedMovie => setSelectedMovie(selectedMovie)}
+                movies={movies}></MovieList>
         </div>
-      )
-    }
-  }
-  
+    )
+}
+
 export default Index;  
